1523ce873069cb8d24d4bfe00109d3a06a10acfb
Keiichiro Nagashima <n4ag3a2sh1i@gmail.com>
2023-02-26 21:53:48 +0900

    fix

--- src/components/TourMap/index.tsx
+++ src/components/TourMap/index.tsx
@@ -16,6 +16,7 @@ import {
   TileLayer,
   useMap,
 } from 'react-leaflet';
+import Modal from 'react-modal';
 
 Leaflet.Icon.Default.imagePath =
   '//cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/';
@@ -38,7 +39,7 @@ export function TourMap({ zoom }: Props) {
   const [current, setCurrent] = useState(0);
   const [timer, setTimer] = useState(false);
   const [delay, setDelay] = useState(100.0);
-  const [searchFound, setSearchFound] = useState(true);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const { register, handleSubmit } = useForm<SearchForm>();
 
   useEffect(() => {
@@ -53,7 +54,7 @@ export function TourMap({ zoom }: Props) {
   const lineResult = useLineRepository();
 
   if (!stationResult.data || !lineResult.data || !tourResult.data) {
-    return <div>error</div>;
+    return <div>loading</div>;
   }
   const tourRepository = tourResult.data;
   const stationRepository = stationResult.data;
@@ -105,16 +106,20 @@ export function TourMap({ zoom }: Props) {
   const onSubmit = (data: SearchForm) => {
     const station = stationRepository.findStationByName(data.query);
     if (!station) {
-      setSearchFound(false);
+      setIsModalOpen(true);
       return;
     }
     const index = tourRepository.getIndexByStationCode(station.station_code);
     if (!index) {
-      setSearchFound(false);
+      setIsModalOpen(true);
       return;
     }
-    setSearchFound(true);
+    setIsModalOpen(false);
     setCurrent(index);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
   };
 
   const center: LatLngTuple = [station.lat, station.lon];
@@ -126,90 +131,104 @@ export function TourMap({ zoom }: Props) {
     tourRepository.getVisitedStationsPercentage(current);
 
   return (
-    <div className={styles.map}>
-      <MapContainer
-        style={{
-          height: '100%',
-          minHeight: '100%',
-        }}
-        center={center}
-        zoom={zoom}
-        scrollWheelZoom={false}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-        />
-        <Marker position={[station.lat, station.lon]}>
-          <Popup>{station.station_name}</Popup>
-        </Marker>
-        <Polyline
-          pathOptions={{ color: 'blue', weight: 3, opacity: 0.5 }}
-          positions={polyline}
-        />
-        <MoveMapCenter position={center} />
-      </MapContainer>
-      <div className={styles.information}>
-        <div className={styles.description}>
-          <div className={styles.station}>{station.station_name}駅</div>
-          <ul className={styles.statusList}>
-            <li className={styles.status}>{line.line_name}</li>
-          </ul>
-          <ul className={styles.statusList}>
-            <li className={styles.status}>
-              {station.prefecture}
-              {station.address}
-            </li>
-          </ul>
-          <ul className={styles.statusList}>
-            <li className={styles.status}>移動回数：{current}</li>
-          </ul>
-          <ul className={styles.statusList}>
-            <li className={styles.status}>
-              訪問済みの駅：
-              {`${visitedStations}（${visitedStationsPercentage.toFixed(2)}%）`}
-            </li>
-          </ul>
+    <>
+      <div className={styles.map}>
+        <MapContainer
+          style={{
+            height: '100%',
+            minHeight: '100%',
+          }}
+          center={center}
+          zoom={zoom}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          />
+          <Marker position={[station.lat, station.lon]}>
+            <Popup>{station.station_name}</Popup>
+          </Marker>
+          <Polyline
+            pathOptions={{ color: 'blue', weight: 3, opacity: 0.5 }}
+            positions={polyline}
+          />
+          <MoveMapCenter position={center} />
+        </MapContainer>
+        <div className={styles.information}>
+          <div className={styles.description}>
+            <div className={styles.station}>{station.station_name}駅</div>
+            <ul className={styles.statusList}>
+              <li className={styles.status}>{line.line_name}</li>
+            </ul>
+            <ul className={styles.statusList}>
+              <li className={styles.status}>
+                {station.prefecture}
+                {station.address}
+              </li>
+            </ul>
+            <ul className={styles.statusList}>
+              <li className={styles.status}>移動回数：{current}</li>
+              <li className={styles.status}>
+                訪問済みの駅：
+                {`${visitedStations}（${visitedStationsPercentage.toFixed(
+                  2,
+                )}%）`}
+              </li>
+            </ul>
+          </div>
+          <div className={styles.control}>
+            <button className={styles.button} onClick={() => moveNext()}>
+              次の駅に進む
+            </button>
+            <button className={styles.button} onClick={() => moveBack()}>
+              前の駅に戻る
+            </button>
+            <button className={styles.button} onClick={() => setTimer(true)}>
+              自動で進む
+            </button>
+            <button className={styles.button} onClick={() => setTimer(false)}>
+              止まる
+            </button>
+            <button className={styles.button} onClick={() => speedUp()}>
+              加速する
+            </button>
+            <button className={styles.button} onClick={() => speedDown()}>
+              減速する
+            </button>
+            <button className={styles.button} onClick={() => reset()}>
+              最初に戻る
+            </button>
+          </div>
+          <div className={styles.search}>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <input
+                className={styles.input}
+                {...register('query')}
+                type='search'
+                placeholder='駅名を検索する'
+              />
+            </form>
+          </div>
         </div>
-        <div className={styles.control}>
-          <button className={styles.button} onClick={() => moveNext()}>
-            次の駅に進む
-          </button>
-          <button className={styles.button} onClick={() => moveBack()}>
-            前の駅に戻る
-          </button>
-          <button className={styles.button} onClick={() => setTimer(true)}>
-            自動で進む
-          </button>
-          <button className={styles.button} onClick={() => setTimer(false)}>
-            止まる
-          </button>
-          <button className={styles.button} onClick={() => speedUp()}>
-            加速する
-          </button>
-          <button className={styles.button} onClick={() => speedDown()}>
-            減速する
-          </button>
-          <button className={styles.button} onClick={() => reset()}>
-            最初に戻る
-          </button>
-        </div>
-        <div className={styles.search}>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <input
-              className={styles.input}
-              {...register('query')}
-              type='search'
-              placeholder='駅名を検索する'
-            />
-          </form>
-          {searchFound ? (
-            ''
-          ) : (
-            <div className={styles.notFound}>見つかりませんでした</div>
-          )}
-        </div>
+        <Modal
+          className={styles.notFound}
+          isOpen={isModalOpen}
+          shouldCloseOnOverlayClick={true}
+          onRequestClose={handleCloseModal}
+          style={{
+            overlay: { zIndex: 100000, backgroundColor: 'rgba(0, 0, 0, 0.5' },
+            content: {
+              margin: '16px',
+              backgroundColor: 'white',
+              padding: '16px',
+              height: '32px',
+            },
+          }}
+        >
+          見つかりませんでした
+        </Modal>
       </div>
-    </div>
+    </>
   );
 }
